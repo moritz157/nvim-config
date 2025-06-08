900bbc6b9db3942855447132e3da99217f751659
moritz157 <moritz.ahrens2@gmail.com>
2025-06-08 13:15:20 +0200

    Switched to new lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,34 +1,37 @@
-local lsp = require("lsp-zero").preset {}
+-- local lsp = require("lsp-zero").preset {}
+--
+-- lsp.ensure_installed {
+--   "eslint",
+--   "gopls",
+-- }
+--
+-- lsp.on_attach(function(client, bufnr)
+--   -- see :help lsp-zero-keybindings
+--   -- to learn the available actions
+--   lsp.default_keymaps { buffer = bufnr }
+-- end)
+--
+-- -- (Optional) Configure lua language server for neovim
+-- require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+--
+-- lsp.setup()
 
-lsp.ensure_installed {
-  "eslint",
-  "gopls",
-}
 
-lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps { buffer = bufnr }
-end)
 
--- (Optional) Configure lua language server for neovim
-require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
-lsp.setup()
-
-local cmp = require "cmp"
-
-cmp.setup {
-  sources = {
-    { name = "nvim_lsp" },
-  },
-  snippet = {
-    expand = function(args)
-      -- You need Neovim v0.10 to use vim.snippet
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ["<C-Space>"] = cmp.mapping.complete {},
-  },
-}
+-- local cmp = require "cmp"
+--
+-- cmp.setup {
+--   sources = {
+--     { name = "nvim_lsp" },
+--   },
+--   snippet = {
+--     expand = function(args)
+--       -- You need Neovim v0.10 to use vim.snippet
+--       vim.snippet.expand(args.body)
+--     end,
+--   },
+--   mapping = cmp.mapping.preset.insert {
+--     ["<C-Space>"] = cmp.mapping.complete {},
+--   },
+-- }
--- init.lua
+++ init.lua
@@ -1,1 +1,24 @@
 require("moritz157")
+
+vim.lsp.enable({ 'luals', 'gopls', 'angularls', 'eslint', 'ts_ls', 'phpactor' })
+
+vim.cmd[[set completeopt+=menuone,noselect,popup]]
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.o.winborder = 'rounded'
+
+vim.diagnostic.config({
+  virtual_lines = true
+})
+-- vim.lsp.config('luals', {
+--   on_init = function()
+--     print('luals now runs in the background')
+--   end,
+-- })
